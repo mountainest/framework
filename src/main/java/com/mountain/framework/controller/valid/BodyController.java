04bfe316f2a58c61d00bed4ccb074df7438317f0
pangjunling <junling.pang@ambergroup.io>
2021-12-06 16:54:01 +0800

    1、Default.class不可省略。 2、只要有一个参数校验失败，就不会往下继续校验。

--- src/main/java/com/mountain/framework/controller/valid/BodyController.java
+++ src/main/java/com/mountain/framework/controller/valid/BodyController.java
@@ -1,6 +1,7 @@
 package com.mountain.framework.controller.valid;
 
 import com.mountain.framework.utils.Response;
+import javax.validation.groups.Default;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -15,7 +16,7 @@ public class BodyController {
     }
 
     @PutMapping("/test")
-    public Response testPut(@Validated(ReqDto.Update.class) @RequestBody ReqDto req) {
+    public Response testPut(@Validated({Default.class, ReqDto.Update.class}) @RequestBody ReqDto req) {
         return Response.success();
     }
 }

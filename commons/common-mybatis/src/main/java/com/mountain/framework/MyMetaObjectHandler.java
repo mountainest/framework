50c02974085e9058e32933c6ae4b837a9996f18c
mountainest <junling.pang@ambergroup.io>
2022-03-21 00:04:29 +0800

    获取用户uid

--- commons/common-mybatis/src/main/java/com/mountain/framework/MyMetaObjectHandler.java
+++ commons/common-mybatis/src/main/java/com/mountain/framework/MyMetaObjectHandler.java
@@ -2,6 +2,7 @@ package com.mountain.framework;
 
 import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
 import java.util.Date;
+import java.util.Optional;
 import org.apache.ibatis.reflection.MetaObject;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,7 +12,7 @@ public class MyMetaObjectHandler implements MetaObjectHandler {
     @Override
     public void insertFill(MetaObject metaObject) {
         Date date = new Date();
-        String uid = "";
+        String uid = this.getUid();
 
         strictInsertFill(metaObject, "creator", String.class, uid);
         strictInsertFill(metaObject, "ctime", Date.class, date);
@@ -22,7 +23,7 @@ public class MyMetaObjectHandler implements MetaObjectHandler {
     @Override
     public void updateFill(MetaObject metaObject) {
         Date date = new Date();
-        String uid = "";
+        String uid = this.getUid();
 
         this.fill(metaObject, uid, date);
     }
@@ -30,5 +31,9 @@ public class MyMetaObjectHandler implements MetaObjectHandler {
     private void fill(MetaObject metaObject, String uid, Date date) {
         strictUpdateFill(metaObject, "updater", String.class, uid);
         strictUpdateFill(metaObject, "utime", Date.class, date);
+    }
+
+    private String getUid() {
+        return SecurityContext.getUid().orElse(null);
     }
 }

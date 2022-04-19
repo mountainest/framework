e25095b0a50f7e72526679aca060e2889eedf3be
mountainest <mountainest@sina.com>
2022-04-19 20:47:37 +0800

    swagger接入完成

--- user-center/src/main/java/io/github/mountainest/Application.java
+++ user-center/src/main/java/io/github/mountainest/Application.java
@@ -4,7 +4,9 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @MapperScan("io.github.mountainest.mapper")
 @EnableFeignClients
 @SpringBootApplication
--- user-center/src/main/java/io/github/mountainest/controller/UserController.java
+++ user-center/src/main/java/io/github/mountainest/controller/UserController.java
@@ -2,6 +2,8 @@ package io.github.mountainest.controller;
 
 import io.github.mountainest.po.UserPo;
 import io.github.mountainest.service.IUserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,12 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
+@Api(tags = "用户管理")
 @RequestMapping("/users")
 @RestController
 public class UserController {
     @Resource
     private IUserService userService;
 
+    @ApiOperation("新增用户")
     @PostMapping()
     public void add(@RequestBody UserPo po) {
         this.userService.add(po);

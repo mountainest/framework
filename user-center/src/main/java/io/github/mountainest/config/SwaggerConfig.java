4f0bb1537f18b90c8effb4dde138c178a6ecc423
mountainest <mountainest@sina.com>
2022-04-22 11:40:59 +0800

    wagger代码整理

--- user-center/src/main/java/io/github/mountainest/config/SwaggerConfig.java
+++ user-center/src/main/java/io/github/mountainest/config/SwaggerConfig.java
@@ -2,14 +2,14 @@ package io.github.mountainest.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
-public class SwaggerConfig {
+public class SwaggerConfig implements WebMvcConfigurer {
     @Bean
     public Docket defaultApi() {
         Docket docket=new Docket(DocumentationType.OAS_30)
@@ -18,5 +18,11 @@ public class SwaggerConfig {
                 .build())
             .groupName("0.0.1版本");
         return docket;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("doc.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }

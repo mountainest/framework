f104e681fb0f551ef6d100811250d002b5cf9724
mountainest <mountainest@sina.com>
2022-05-04 18:01:49 +0800

    多个错误码的文件拼装

--- commons/common-utils/src/main/java/io/github/mountainest/MessageUtils.java
+++ commons/common-utils/src/main/java/io/github/mountainest/MessageUtils.java
@@ -1,27 +1,16 @@
 package io.github.mountainest;
 
+import io.github.mountainest.config.SpringContextUtil;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
-import org.springframework.context.support.ResourceBundleMessageSource;
 
 public class MessageUtils {
-    private static MessageSource messageSource;
-    static {
-        messageSource = getMessageSource();
-    }
-
-    public static ResourceBundleMessageSource getMessageSource() {
-        ResourceBundleMessageSource bundleMessageSource = new ResourceBundleMessageSource();
-        bundleMessageSource.setDefaultEncoding("UTF-8");
-        bundleMessageSource.setBasenames("i18n/messages");
-        return bundleMessageSource;
-    }
-
     public static String getMessage(int code){
         return getMessage(code, new String[0]);
     }
 
     public static String getMessage(int code, String... params){
+        MessageSource messageSource = SpringContextUtil.getBean(MessageSource.class);
         return messageSource.getMessage(code+"", params, LocaleContextHolder.getLocale());
     }
 }
---
+++ commons/common-utils/src/main/java/io/github/mountainest/config/SpringContextUtil.java
@@ -0,0 +1,20 @@
+package io.github.mountainest.config;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringContextUtil implements ApplicationContextAware {
+    private static ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        SpringContextUtil.applicationContext = applicationContext;
+    }
+
+    public static <T> T getBean(Class<T> clz) {
+        return SpringContextUtil.applicationContext.getBean(clz);
+    }
+}

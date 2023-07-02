0bc6c80da5bf23966309ad5b1f70819f1ea77ee7
mountain <mountainest@sina.com>
2023-07-02 23:24:21 +0800

    用户信息查询

--- friends/src/main/java/io/github/mountainest/controller/UserController.java
+++ friends/src/main/java/io/github/mountainest/controller/UserController.java
@@ -1,4 +1,33 @@
 package io.github.mountainest.controller;
 
+import io.github.mountainest.Result;
+import io.github.mountainest.dto.UserDto;
+import io.github.mountainest.dto.UserQuery;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Api(tags = "用户资料")
+@RequestMapping("/users")
+@RestController
 public class UserController {
+    @ApiOperation("新增用户资料")
+    @PostMapping()
+    public Result<Void> add(UserDto dto) {
+        return Result.success();
+    }
+
+    @ApiOperation("修改用户资料")
+    @PutMapping()
+    public Result<Void> update(UserDto dto) {
+        return Result.success();
+    }
+
+    @ApiOperation("查找用户资料")
+    @GetMapping()
+    public Result<List<UserDto>> list(UserQuery query) {
+        return Result.success();
+    }
 }
---
+++ friends/src/main/java/io/github/mountainest/dto/UserQuery.java
@@ -0,0 +1,24 @@
+package io.github.mountainest.dto;
+
+import lombok.Data;
+
+@Data
+public class UserQuery {
+    private String location;
+    private Boolean femaleFlg;
+    private Integer minBirthday;
+    private Integer maxBirthday;
+    private Short minHeight;
+    private Short maxHeight;
+    private Short weight;
+    private Short minWeight;
+    private Short maxWeight;
+
+    private Short minAnnualSalary;
+    private Short companyType;
+    private String job;
+    private Short education;
+
+    private String province;
+    private String city;
+}

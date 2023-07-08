8dd26e08094db58d1de9602e685fb4cef29e93ad
mountain <mountainest@sina.com>
2023-07-08 21:50:38 +0800

    springfox

--- friends/src/main/java/io/github/mountainest/FriendsApplication.java
+++ friends/src/main/java/io/github/mountainest/FriendsApplication.java
@@ -2,7 +2,10 @@ package io.github.mountainest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+// springfox
+@EnableWebMvc
 @SpringBootApplication
 public class FriendsApplication {
     public static void main(String[] args) {

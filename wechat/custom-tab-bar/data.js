677af015ca75a405016738f2969c8ee66de75678
mountain <mountainest@sina.com>
2024-06-17 21:14:06 +0800

    删除无用的tab页

--- wechat/custom-tab-bar/data.js
+++ wechat/custom-tab-bar/data.js
@@ -5,18 +5,8 @@ export default [
     url: 'pages/home/home',
   },
   {
-    icon: 'sort',
-    text: '分类',
-    url: 'pages/goods/category/index',
-  },
-  {
-    icon: 'cart',
-    text: '购物车',
-    url: 'pages/cart/index',
-  },
-  {
     icon: 'person',
-    text: '个人中心',
+    text: '我的',
     url: 'pages/usercenter/index',
   },
 ];

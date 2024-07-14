71985a4c48c66c6e6b1fa76ed74c7a8b86287631
mountain <mountainest@sina.com>
2024-07-14 16:38:58 +0800

    登录

--- wechat/app.js
+++ wechat/app.js
@@ -1,7 +1,20 @@
 import updateManager from './common/updateManager';
 
 App({
-  onLaunch: function () {},
+  onLaunch: function () {
+    this.login();
+  },
+  login: function() {
+    wx.login({
+      success: (res) => {
+        // code = res.code;
+        console.log("登录成功。", res.errMsg);
+      },
+      fail: (res) => {
+        console.log("登录失败。", res.errMsg);
+      }
+    })
+  },
   onShow: function () {
     updateManager();
   },
